a4a831c3e000aaec4adff8f5344fe8b8e82f5710
HexTeke <yianni.whisler@gmail.com>
2023-12-07 02:08:25 -0600

    rework search component

---
+++ components/Search.vue
@@ -0,0 +1,131 @@
+<script setup>
+const store = useStore();
+
+const searchField = ref(null);
+const searchValue = ref("");
+const searchError = ref("");
+
+onMounted(() => {
+  nextTick(() => {
+    searchField.value.focus();
+  });
+});
+
+async function search(input) {
+  const inputResult = input.split("#");
+  
+  store.setIsLoading(true);
+  try {
+    const response = await $fetch('/api/requestUser', {
+      method: 'POST',
+      body: {
+        "GameName": inputResult[0],
+        "TagLine": inputResult[1]
+      }
+    });
+    await store.setUser(response);
+    await navigateTo({ path: `/users/${inputResult[0]}-${inputResult[1]}` });
+  } catch(e) {
+    searchError.value = e.message;
+  }
+  store.setIsLoading(false);
+}
+
+const resetError = () => searchError.value = "";
+</script>
+
+<template>
+  <div>
+    <div id="search-container" class="flex-row">
+      <div id="input-container" class="align-center">
+        <Icon class="input-icon pl-2 pr-1" name="mdi:search" color="black" />
+        <input
+          ref="searchField"
+          v-model="searchValue"
+          @keydown.enter="search(searchValue)"
+          @click="resetError()"
+          placeholder="GameName#Tagline" />
+      </div>
+      <button id="search-button" @click="search(searchValue)">
+        <Icon name="material-symbols:expand-circle-right-outline" color="aliceblue" size="32px"/>
+      </button>
+    </div>
+
+    <Transition appear name="slide-fade">
+      <div v-if="searchError" id="search-error" class="pt-4 text-center">
+        {{ searchError }}
+      </div>
+    </Transition>
+
+    <button @click="search('Yianni#9052')">test</button>
+  </div>
+</template>
+
+<style scoped>
+#search-container {
+  height: 45px;
+  width: 100%;
+
+  outline: 2px solid var(--neutral);
+  border-radius: 10px;
+  transition: outline 0.225s;
+
+  &:hover {
+    outline: 2px solid var(--primary-light);
+    border-right: none;
+  }
+
+  &:focus-within {
+    outline: 2px solid var(--border-focus);
+    border-right: none;
+    box-shadow: var(--shadow);
+  }
+}
+
+#input-container {
+  width: 100%;
+
+  border-top-left-radius: 10px;
+  border-bottom-left-radius: 10px;
+
+  display: flex;
+  background-color: var(--font-color);
+
+  & input {
+    all: unset;
+    color: var(--border);
+    width: 100%;
+  }
+}
+
+.input-icon {
+  color: black;
+  pointer-events: none;
+  text-align: center;
+
+  min-width: 24px;
+  max-width: 24px;
+  min-height: 24px;
+  max-height: 24px;
+}
+
+#search-button {
+  width: 80px;
+
+  border: none;
+  border-top-right-radius: 10px;
+  border-bottom-right-radius: 10px;
+
+  background-color: var(--border-focus-less);
+  font-family: 'Nunito Sans', sans-serif;
+  font-size: larger;
+
+  cursor: pointer;
+}
+
+#search-error {
+  color: red;
+  text-shadow: var(--shadow);
+}
+
+</style>
